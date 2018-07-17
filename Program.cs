8e7748906a063625fd28c51259e3702738d019a2
nsenelier <nsenelier@gmail.com>
2018-07-17 11:31:57 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace learning
+{
+    class Program
+    {
+         public static void Main(string[] args) {
+            ///Example online
+            StringBuilder message  = new StringBuilder();
+
+            if(args.Length < 1) {
+                message.Append("Welcome to .Net Core!");
+            }
+            else {
+                foreach(string item in args ){
+                    message.Append(item); //TODO replace with String builder
+                }
+            }
+
+            Console.WriteLine(message);
+
+        }
+        
+    }
+}
